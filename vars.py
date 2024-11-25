3976e2f2a2ba29f503f3b2cf7c85bfa228e18cc5
krishna931074 <kumarrohit92572@gmail.com>
2024-11-25 21:22:36 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -7,4 +7,3 @@ from os import environ
 API_ID = int(environ.get("API_ID", "21567814"))
 API_HASH = environ.get("API_HASH", "cd7dc5431d449fd795683c550d7bfb7e")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-THUMBNAIL_ID =int(environ.got("THUMBNAIL_ID", "20241122185713"))
