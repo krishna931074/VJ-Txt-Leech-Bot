63317a86218a9d777ec17d711e04c2630fe1964e
STRANGER BOY 💔 BROKEN 👦 <kumarrohit92572@gmail.com>
2025-01-20 20:05:00 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -4,6 +4,6 @@
 
 from os import environ
 
-API_ID = int(environ.get("API_ID", "22182189"))
-API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "20607064"))
+API_HASH = environ.get("API_HASH", "c0a09fd762681a66366cf84976f31a17")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7939391364:AAFqi5QuudQtJGoBTYbnTHgqUu_Kd6UF5EY")
